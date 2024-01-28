8a536cdb88ec4c2dad3bb12737578f4a9d0e477c
Dildora1140 <157961522+Dildora1140@users.noreply.github.com>
2024-01-28 22:20:16 +0500

    Server
    server bilan boglanishda ozgartirish kiriyildi

--- lib/yangiproyekt.dart/Api/request.dart
+++ lib/yangiproyekt.dart/Api/request.dart
@@ -5,7 +5,7 @@ class XabarniYukla {
   static var data;
   Future xabarniOl() async {
     var sayt = Uri.parse(
-      'https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/team',
+      'https://heisenbug-premier-league-live-scores-v1.p.rapidapi.com/api/premierleague/team/',
     );
     final sorov = await http.get(sayt, headers: {
       'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
